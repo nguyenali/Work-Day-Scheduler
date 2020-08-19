8b6e60e0aab645eddd9db4c93e24fcda5e561751
Ali Nguyen <alin57717@gmail.com>
2020-08-18 23:43:16 -0700

    add statement to load data from local storage

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+//Load saved data from LocalStorage
+$("#9am.description").val(localStorage.getItem("9am"));
+$("#10am.description").val(localStorage.getItem("10am"));
+$("#11am.description").val(localStorage.getItem("11am"));
+$("#12pm.description").val(localStorage.getItem("12pm"));
+$("#1pm.description").val(localStorage.getItem("1pm"));
+$("#2pm.description").val(localStorage.getItem("2pm"));
+$("#3pm.description").val(localStorage.getItem("3pm"));
+$("#4pm.description").val(localStorage.getItem("4pm"));
+$("#5pm.description").val(localStorage.getItem("5pm"));
+
+
+
+function hourTracker () {
+
+    var currenthour = moment().hour();
+
+
+
+
+    if(blockHour<currenthour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        }
+
+    else if (blockHour===currenthour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        }
+
+
+    else {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        }
+    }
+}
