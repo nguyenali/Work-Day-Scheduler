7d963d13b2287fcf58a42f81b65b0ac6cbb4aea0
Ali Nguyen <alin57717@gmail.com>
2020-08-22 22:27:30 -0700

    corrected local storage issue by fixing closing brackets in js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 
-    $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
     $(".saveBtn").on("click", function() {
         console.log(this);
         var text= $(this).siblings(".description").val();
         var time =$(this).parent().attr("id");
 
-        localStorage.setItem(time,text);
+        localStorage.setItem(time, text);
     })
 
 
@@ -17,27 +17,27 @@ $(document).ready(function() {
 
 
 
-//Load saved data from LocalStorage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+    //Load saved data from LocalStorage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
 
 
 
-function hourTracker() {
+    function hourTracker() {
 
-    var currenthour = moment().hour();
+        var currenthour = moment().hour();
 
 
-    $(".time-block").each(function () {
+        $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
         console.log(blockHour,currenthour)
 
@@ -60,8 +60,8 @@ function hourTracker() {
             $(this).addClass("future");
             $(this).removeClass("present");
             }
-        }
-    })
+        })
+    }
 
 
 
