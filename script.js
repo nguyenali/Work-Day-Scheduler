48db9d241c0a2b8e84b44bb98219f7374ed737af
Ali Nguyen <alin57717@gmail.com>
2020-08-22 18:51:24 -0700

    fixed error in 1pm save button html

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ $(document).ready(function() {
 
 
 //Load saved data from LocalStorage
-$("#9th-hour.description").val(localStorage.getItem("9th-hour"));
-$("#10th-hour.description").val(localStorage.getItem("10th-hour"));
-$("#11th-hour.description").val(localStorage.getItem("11th-hour"));
-$("#12th-hour.description").val(localStorage.getItem("12th-hour"));
-$("#13th-hour.description").val(localStorage.getItem("13th-hour"));
-$("#14th-hour.description").val(localStorage.getItem("14th-hour"));
-$("#15th-hour.description").val(localStorage.getItem("15th-hour"));
-$("#16th-hour.description").val(localStorage.getItem("16th-hour"));
-$("#17th-hour.description").val(localStorage.getItem("17th-hour"));
+$("#9th-hour .description").val(localStorage.getItem("9th-hour"));
+$("#10th-hour .description").val(localStorage.getItem("10th-hour"));
+$("#11th-hour .description").val(localStorage.getItem("11th-hour"));
+$("#12th-hour .description").val(localStorage.getItem("12th-hour"));
+$("#13th-hour .description").val(localStorage.getItem("13th-hour"));
+$("#14th-hour .description").val(localStorage.getItem("14th-hour"));
+$("#15th-hour .description").val(localStorage.getItem("15th-hour"));
+$("#16th-hour .description").val(localStorage.getItem("16th-hour"));
+$("#17th-hour .description").val(localStorage.getItem("17th-hour"));
 
 
 
@@ -42,13 +42,13 @@ function hourTracker () {
         console.log(blockHour,currenthour)
 
 
-        if(blockHour<currenthour) {
+        if(blockHour < currenthour) {
             $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
             }
     
-        else if (blockHour===currenthour) {
+        else if (blockHour === currenthour) {
             $(this).removeClass("past");
             $(this).removeClass("future");
             $(this).addClass("present");
